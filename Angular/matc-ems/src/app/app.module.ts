f3f4130f101e4991323e69e6f2a2a88cab7f8bad
Abinesh <abinesh.dorustree>
2023-04-10 16:37:35 +0530

    PWA - init

--- Angular/matc-ems/src/app/app.module.ts
+++ Angular/matc-ems/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +9,8 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { FormsModule } from '@angular/forms';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import {environment} from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -24,7 +26,13 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      // registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Angular/matc-ems/src/app/login/login.component.ts
+++ Angular/matc-ems/src/app/login/login.component.ts
@@ -33,8 +33,26 @@ export class LoginComponent {
 
   login() {
     for (let i = 0; i < this.localStorageData.length; i++) {
-      if (this.userMail === this.localStorageData[i].userEmail && this.userPassword === this.userPassword) {
-        this.router.navigate(['/dashboard']);
+      if (this.userMail == '' || this.userPassword == '') {
+        alert('The fields should not be empty !');
+        return;
+      } else {
+        if (this.userMail === this.localStorageData[i].userEmail) {
+          if (this.userPassword === this.localStorageData[i].userPassword) {
+            console.log('Login sucessfull');
+            alert("Login successful");
+            this.router.navigate(['/dashboard']);
+            return;
+          } else {
+            alert('Invalid password');
+            this.router.navigate(['/login']);
+            return;
+          }
+        } else {
+          alert('Invalid login credentials');
+          this.router.navigate(['/login']);
+          return
+        }
       }
     }
   }
